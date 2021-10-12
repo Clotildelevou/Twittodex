dc0527f78d3784a7f08a1a2fbccfeb04723273d0
Clotilde LEVESQUE <clotilde.levesque@epita.fr>
2021-10-12 11:33:18 +0200

    [FEAT] Secured credentials now

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import tweepy
+import csv
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 
 
+# returns the connected api
 def get_api(consumer_key, consumer_secret, access_token, access_token_secret):
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
@@ -14,6 +19,7 @@ def get_api(consumer_key, consumer_secret, access_token, access_token_secret):
         print("Error during authentication")
 
 
+# cleans 200 tweets of the bot
 def delete_all_tweets(api):
     tweets = api.user_timeline(screen_name="the_gengar_bot",
                                # 200 is the maximum allowed count
@@ -29,6 +35,7 @@ def generate_reply(sentence):
     return reply
 
 
+# retweets
 def retweet_gengar(api):
     for tweet in api.search(q="Gengar", lang="en", rpp=2):
         tweet_to_quote_url = "https://twitter.com/twitter/statuses/" + str(tweet.id)
@@ -37,10 +44,14 @@ def retweet_gengar(api):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    consumer_key = "RBuhlvOgdwEyLpQP6VBbQbj1q"
-    consumer_secret = "bsNTSEwe189izP5IAz781WIFIs9DuDSyxdOeRjrohSulFnAYeB"
-    access_token = "1441322507491942400-NOQVsJ36GCKMba6mNCriRWfWUrM3P7"
-    access_secret = "6RREaEwToJe89xVpoUdo45PKhhTYdGVmvtGm5SV8mSMux"
+    # gets the credentials
+    dotenv_path = join(dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+    CONSUMER_KEY = os.environ.get("CONSUMER_KEY")
+    CONSUMER_SECRET = os.environ.get("CONSUMER_SECRET")
+    ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
+    ACCESS_SECRET = os.environ.get("ACCESS_SECRET")
 
-    api = get_api(consumer_key, consumer_secret, access_token, access_secret)
-    delete_all_tweets(api)
+
+    # connects the api
+    api = get_api(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
