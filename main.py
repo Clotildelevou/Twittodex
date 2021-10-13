bb3fad9ec126866da4c503236c953c51f918349a
Clotilde LEVESQUE <clotilde.levesque@epita.fr>
2021-10-13 14:13:14 +0200

    [FEAT] post every 12 hours

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
+from random import randint
+import logger
+import time
+
 import authentification
-import pokemon
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # gets the credentials
-    pokemon.build_weaknesses(15)
+
+    posted_list = []
+    logfile = logger.gen_logfile()
+    while True:
+        if len(posted_list) != 781:
+            national_number = randint(0, 780)
+
+            while national_number in posted_list:
+                national_number = randint(0, 780)
+
+            authentification.gen_daily_tweet(national_number, logfile)
+            authentification.set_profile_picture(national_number)
+            posted_list.append(national_number)
+        else:
+            api = authentification.get_api()
+            api.update_status("I made my job. now I have to leave,thank you everyone for checking on me !")
+            break
+        time.sleep(43200)
