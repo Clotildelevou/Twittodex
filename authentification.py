629483ed26589d5578cf3231e11fad2554643817
Clotilde LEVESQUE <clotilde.levesque@epita.fr>
2021-10-13 14:12:46 +0200

    [FEAT] aily tweet new features

--- authentification.py
+++ authentification.py
@@ -35,13 +35,43 @@ def set_profile_picture(national_number):
 
 
 # generate the daily tweet
-def gen_daily_tweet(national_number):
+def gen_daily_tweet(national_number, logfile):
     api = get_api()
-    description = pokemon.build_desc(national_number)
+    name = pokemon.get_pokemon_info(national_number).english_name
+    description = pokemon.build_description(national_number)
     img_path = pokemon.get_pokemon_pic(national_number)
     stats_path = pokemon.get_pokemon_stats(national_number)
+    weak_path = pokemon.get_pokemon_weakness(national_number)
+    status = 0
 
-    status = api.update_with_media(img_path, description)
-    api.update_with_media(stats_path, "Stats of today's Pokemon", in_reply_to_status_id=status.id,
-                          auto_populate_reply_metadata=True)
+    # First post
+    try:
+        status = api.update_with_media(img_path, description)
+        logger.pokemon_posted(logfile, name)
+    except:
+        error = traceback.format_exc()
+        logger.err_pokemon_posted(logfile, name, error)
 
+    # stats comment
+    if not os.path.exists(stats_path):
+        pokemon.build_stats(national_number)
+
+    try:
+        status = api.update_with_media(stats_path, "Stats of today's Pokemon", in_reply_to_status_id=status.id,
+                                       auto_populate_reply_metadata=True)
+        logger.stats_posted(logfile, name)
+    except:
+        error = traceback.format_exc()
+        logger.err_stats_posted(logfile, name, error)
+
+    # weaknesses comment
+    if not os.path.exists(weak_path):
+        pokemon.build_weaknesses(national_number)
+
+    try:
+        api.update_with_media(weak_path, "Weaknesses of today's Pokemon", in_reply_to_status_id=status.id,
+                              auto_populate_reply_metadata=True)
+        logger.stats_posted(logfile, name)
+    except:
+        error = traceback.format_exc()
+        logger.err_stats_posted(logfile, name, error)
